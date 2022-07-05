52e1cf3f07653d5ef96ded948b80e6541fe0a6f1
tugalsan karabacak <tugalsan@gmail.com>
2022-07-05 09:22:14 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.tugalsan.trm.recorder {
+    requires gwt.user;
+    requires java.desktop;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.random;
+    requires com.tugalsan.api.thread;
+    requires com.tugalsan.api.input;
+    requires com.tugalsan.api.time;
+    requires com.tugalsan.api.cast;
+    requires com.tugalsan.api.file;
+}
