8017d6e942d26b47943fbf170dc017198f4e11cd
tugalsan karabacak <tugalsan@gmail.com>
2022-08-29 12:17:18 +0300

    api.log dep code update

--- src/main/java/com/tugalsan/trm/paint/Main.java
+++ src/main/java/com/tugalsan/trm/paint/Main.java
@@ -5,7 +5,7 @@ import com.tugalsan.api.log.server.*;
 //WHEN RUNNING IN NETBEANS, ALL DEPENDENCIES SHOULD HAVE TARGET FOLDER!
 public class Main {
 
-    final private static TS_Log d = TS_Log.of(Main.class.getSimpleName());
+    final private static TS_Log d = TS_Log.of(Main.class);
 
     public static void main(String[] args) {
         new Scribble().setVisible(true);
